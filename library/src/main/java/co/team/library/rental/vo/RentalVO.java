b39d7da690ab9c8413573b6cbcba24b7a94a3eed
admin <admin@YD2-01>
2022-06-15 11:58:17 +0900

    220615

---
+++ library/src/main/java/co/team/library/rental/vo/RentalVO.java
@@ -0,0 +1,13 @@
+package co.team.library.rental.vo;
+
+import java.sql.Date;
+
+public class RentalVO {
+	private String id;
+	private String bookCode;
+	private Date rentalDate;
+	private Date returnDate;
+	private int returnBook;
+	private int returnComplOrNot;
+	
+}
